b919c6084f3530d7486c26f6689093e95f3f5f6b
brainer <brainer@kakao.com>
2022-08-06 17:23:39 +0900

    Add verticalScroll for MainView

--- app/src/main/java/com/moeumi/client/MainActivity.kt
+++ app/src/main/java/com/moeumi/client/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,8 +33,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainView() {
-    Column {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
         MainBanner()
+        MainCategoryGroup()
+        MainList()
     }
 }
 
