0caae6bbfd84d21a9fdbeffb8285246222d5a538
brainer <brainer@kakao.com>
2022-08-09 10:41:05 +0900

    Add notosans font

---
+++ app/src/main/java/com/moeumi/client/Font.kt
@@ -0,0 +1,10 @@
+package com.moeumi.client
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val notoSanse = FontFamily(
+    Font(R.font.notosanskr_black, FontWeight.Black),
+    Font(R.font.notosanskr_medium, FontWeight.Medium),
+)
