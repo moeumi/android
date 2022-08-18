604184878820397c8b3e6cf7f610f954393ef844
brainer <brainer@kakao.com>
2022-08-18 15:23:32 +0900

    Add notoSanse Bold

--- app/src/main/java/com/moeumi/client/Font.kt
+++ app/src/main/java/com/moeumi/client/Font.kt
@@ -7,4 +7,5 @@ import androidx.compose.ui.text.font.FontWeight
 val notoSanse = FontFamily(
     Font(R.font.notosanskr_black, FontWeight.Black),
     Font(R.font.notosanskr_medium, FontWeight.Medium),
+    Font(R.font.notosanskr_bold, FontWeight.Bold),
 )
